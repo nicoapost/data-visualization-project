4eff8b5c1146c37698b8c5faaa8e3d3abb362bd6
Nico Apostolopoulos <nico.apostolopoulos@rheaply.com>
2020-11-10 15:59:53 -0600

    broken, csv not working

--- scripts/histogram.js
+++ scripts/histogram.js
@@ -13,29 +13,30 @@ var svg = d3.select("#histogram")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // get the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
+d3.csv("../data/hist.csv", function(data) {
 
   // X axis: scale and draw:
   var x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .domain([0, 23])
       .range([0, width]);
+
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x).ticks(24));
 
   // set the parameters for the histogram
   var histogram = d3.histogram()
-      .value(function(d) { return d.price; })   // I need to give the vector of value
+      .value(function(d) { return d.users; })   // I need to give the vector of value
       .domain(x.domain())  // then the domain of the graphic
-      .thresholds(48); // then the numbers of bins
+      .thresholds(24); // then the numbers of bins
 
   // And apply this function to data to get the bins
   var bins = histogram(data);
 
   // Y axis: scale and draw:
   var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+      .domain([0, 101000])
+      .range([height, 0]);   // d3.hist has to be called before the Y axis obviously
   svg.append("g")
       .call(d3.axisLeft(y));
 
